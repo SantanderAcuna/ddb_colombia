2ad5c70a11c6fd7e9678375767f1d7e43e0f1263
Jose acuña <panson@misena.edu.co>
2023-08-06 18:24:38 -0500

    se crean las funciones del sweetalert

--- frontend/src/funciones.js
+++ frontend/src/funciones.js
@@ -2,8 +2,7 @@ import Swal from "sweetalert2";
 import axios from "axios";
 
 export function mostrarAlerta(titulo, icono, foco = '') {
-
-    if (foco != '') {
+    if (foco !== '') {
         document.getElementById(foco).focus();
     }
 
@@ -11,48 +10,67 @@ export function mostrarAlerta(titulo, icono, foco = '') {
         title: titulo,
         icon: icono,
         customClass: {
-            confirmButton: 'btn btn-primary', popup: 'animated zoonIn',
+            confirmButton: 'btn btn-primary',
+            popup: 'animated zoonIn',
             buttonsStyling: false
-
         }
     });
-
 }
 
 export function confirmar(urlSlash, id, titulo, icono, mensaje) {
-
     var url = urlSlash + id;
-    const swalWithBootstrapButton = swal.mixin({
-
+    const swalWithBootstrapButton = Swal.mixin({
         customClass: {
-            confirmButton: 'btn btn-success me-3', cancelButton: 'btn btn-danger',
-        },
-
-
+            confirmButton: 'btn btn-success me-3',
+            cancelButton: 'btn btn-danger'
+        }
     });
 
     swalWithBootstrapButton.fire({
         title: titulo,
-        text:mensaje,
+        text: mensaje,
         icon: 'question',
-        showCancelButton:true,
-        confirmButtonText:' <i class="fa-solid fa-check"></i> Si, eliminar',
-        cancelButtonText:'<i class="fa-solid fa-ban"></i> Cancelar'}).then((res)=>{
-
-            
-        })
-
-    }
-    
+        showCancelButton: true,
+        confirmButtonText: ' <i class="fa-solid fa-check"></i> Si, eliminar',
+        cancelButtonText: '<i class="fa-solid fa-ban"></i> Cancelar'
+    }).then((res) => {
+        if (res.isConfirmed) {
+            enviarSolicitud('DELETE', { id: id }, url, 'Eliminado con exito');
+        } else {
+            mostrarAlerta('Operacion cancelada', 'info');
+        }
+    });
 
     Swal.fire({
         title: titulo,
         icon: icono,
         customClass: {
-            confirmButton: 'btn btn-primary', popup: 'animated zoonIn',
+            confirmButton: 'btn btn-primary',
+            popup: 'animated zoonIn',
             buttonsStyling: false
+        }
+    });
+}
+
+export function enviarSolicitud(metodo, parametros, url, mensaje) {
+
+    axios({ method: metodo, url: url, data: parametros }).then((res) => {
+
+        var estado = res.data.estado;
+        if (estado == 200) {
+            mostrarAlerta(mensaje, 'success');
+            window.setTimeout(() => {
+                window.location.href = '/';
+            }, 1000);
+        } else {
+
+            mostrarAlerta('No se pudo recuperar la respuesta', 'error');
+
 
         }
-    }),
+    }).catch((error)=>{
+
+        mostrarAlerta('Servidor no disponible', 'error')
+    })
 
 }
